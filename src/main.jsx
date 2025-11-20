d06bf2691cedb919c996c4ae1a91848df822488e
CreativeMarkus <mark@creativemarkus.com>
2025-11-20 12:56:25 -0500

    Add Atatus monitoring integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+import * as atatus from 'atatus-spa';
+atatus.config('b875923400ba4f3ca280fa6a31e61ea7').install();
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
